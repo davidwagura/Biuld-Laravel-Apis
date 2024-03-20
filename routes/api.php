cbb0a4972c04d641128fc531d585dfc21325d059
David wanjohi <davidwagura83@gmail.com>
2024-03-20 10:56:12 +0300

    routing

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+
+Route::resource('/tasks', TaskController::class);
